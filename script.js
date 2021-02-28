ff2f55ab2c08cb6da50a27e71fb705feae977cfe
Theresa Pidutti <treepidutti@gmail.com>
2021-02-28 11:44:55 -0700

    update password homework

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+
 function generatePassword() { 
+
+  // Password must be mix of special characters, numbers, lower and upper case letters
 var myNumbers = [0,1,2,3,4,5,6,7,8,9];
-
 var mySpecial = ["!()*+,-./:;<=>?@[\]^_`{|}~"];
-
 var myLowerCharacters = ["a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z"]
-
 var myUpperCharacters = ["A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z.A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z."]
 
 
-  var combinePassword =(myNumbers, mySpecial, myLowerCharacters, myUpperCharacters);
-  combinePassword.join();
-  console.log(combinePassword);
-
+  // var combinePassword = myNumbers.concat ( mySpecial, myLowerCharacters, myUpperCharacters);
+  // // combinePassword.join();
+  // console.log(combinePassword);
 }
 
 // // Write password to the #password input
@@ -26,19 +26,30 @@ function writePassword() {
 }
 
 
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function inTheBox(){
-  var publish = document.getElementById("password");
+
+// Password must be displayed/written in the box, replacing the words your secure password
+
+// function inTheBox(){
+//   var publish = document.getElementById("password");
   
-  if(publish.innerText === ""){
-    publish.innerText === "";
-  } else {
-    publish.innerText = "";
-  }
-}
+//   if(publish.innerText === ""){
+//     publish.innerText === "";
+//   } else {
+//     publish.innerText = "";
+//   }
+// }
+
+
+// Prompt for password criteria that is user selected
+// Prompt for length of password between 8 and 128 characters
+
+// All prompts must be answered, each prompt checked for valid entry and type of character that meets criteria. if prompt meets criteria then display, else prompt for correct character type, length.
+// Password must be randomly generated
+// Password must match criteria
+
 
 // function passwordHere() {
 //   var accessPassword = document.getElementById("password");
@@ -46,11 +57,3 @@ function inTheBox(){
 // }
 
 
-
-// Prompt for password criteria that is user selected
-// Prompt for length of password between 8 and 128 characters
-// Prompt for mix of special characters, numbers, lower and upper case letters
-// All prompts must be answered, each prompt checked for valid entry and type of character that meets criteria. if prompt meets criteria then display, else prompt for correct character type, length.
-// Password must be randomly generated
-// Password must match criteria
-// Password must be displayed/written in the box, replacing the words your secure password
